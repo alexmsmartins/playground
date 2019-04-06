ac6f2b1dbdef2bf188ff16fec329218d65898da6
Alexandre Martins <alexmsmartins@gmail.com>
2019-04-06 21:49:57 +0100

    A script now works with stack and the lts-13.15 resolver.

--- learn_haskell/scripting/hello-script.hs
+++ learn_haskell/scripting/hello-script.hs
@@ -1,5 +1,19 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-13.15 --install-ghc runghc
-main = do
-  putStrLn "Hello"
+import Data.Char
+import Data.List
+import qualified Data.Map.Strict as M
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import System.Environment
 
+main = do
+  [fp] <- getArgs
+  text <- T.readFile fp
+  let histo =
+        foldl'
+          (\hash line -> M.insertWith (+) (T.length line) 1 hash)
+          M.empty
+          (T.lines text)
+  mapM_ (\(key, value) -> print (key, value)) (M.toList histo)
+  putStrLn "Hello"
